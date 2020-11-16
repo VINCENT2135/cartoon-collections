0bc9e73596fd148cddf6bff5cab2d5a54b280978
Vincent Ivos <vincentivos1@gmail.com>
2020-11-15 22:41:15 -0500

    commit tree 8d20965a7e07eb2420227d8be44e1aa9feaf085f

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)# code an argument here
+  dwarves.each_with_index do |dwarves, index|
+    index += 1
+    puts "#{index}. #{dwarves}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet(calls) # code an argument here
+ calls.collect do |call|
+    call.capitalize << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls (calls)
+  calls.any? do |item|
+    item.size > 4
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(type) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  type.find do |flavor|
+    cheese_types.include?(flavor)
+  end
 end
